c1f0ad7c6e433241765ba4b4fdf866b60d000b9f
Hephzibah <94688043+hephzi06@users.noreply.github.com>
2022-07-14 19:26:28 +0100

    made some changes

--- src/Content.js
+++ src/Content.js
@@ -1,30 +1,19 @@
-import React from 'react'
+import ItemList from "./ItemList";
 
-const Content = () => {
-    const handleNameChange = () =>{
-        const names = ['Bob', 'Kevin', 'Dave'];
-        const int  = Math.floor(Math.random() * 3);
-        return names[int];
-      } 
-      const handleClick = () =>{
-        alert('You clicked it ')
-      }
-      const handleClick2 = (name) =>{
-        alert( `${name} was clicked`)
-      }
-      const handleClick3 = (e) =>{
-        console.log(e.target.innerText)
-      }
-    return (
-        <main>
-            <p onDoubleClick={handleClick}>
-                Hello {handleNameChange()}!
-            </p>
-            <button onClick={handleClick}>click it</button>
-            <button onClick={() => {handleClick2('Dave')}}>click it</button>
-            <button onClick={(e) => {handleClick3(e)}}>click it</button>
-        </main>
-    )
-}
+const Content = ({ items, handleCheck, handleDelete }) => {
+  return (
+    <main>
+      {items.length ? (
+        <ItemList
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
+      ) : (
+        <p style={{ marginTop: "2rem" }}>Your list is empty </p>
+      )}
+    </main>
+  );
+};
 
-export default Content 
+export default Content;
--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const Header = () => {
-        return(
-       <header>
-        <h1>Groceries List</h1>
-       </header >
-  )
-}
+const Header = ({ title }) => {
+  return (
+    <header>
+      <h1>{title}</h1>
+    </header>
+  );
+};
+Header.defaultProps = {
+  title: "Default Title",
+};
 
-
-export default Header
+export default Header;
